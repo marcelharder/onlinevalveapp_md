f83f0fc196fb841d0bab71366d21d52ddb108efd
marcel <marcelharder@protonmail.com>
2021-05-28 10:24:09 +0200

    works

--- spa/src/app/companyadmin/editProduct/editProduct.component.ts
+++ spa/src/app/companyadmin/editProduct/editProduct.component.ts
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
 import { ValveService } from 'src/app/_services/valve.service';
 import { FileUploader } from 'ng2-file-upload';
 import { environment } from 'src/environments/environment';
+import { valveSize } from 'src/app/_models/valveSize';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -18,6 +19,8 @@ export class EditProductComponent implements OnInit {
   @Output() povOut: EventEmitter<number> = new EventEmitter();
   ImagePath = "";
   baseUrl = environment.apiUrl;
+  showAdd = 0; newsize = 0; neweoa = 0.0;
+  valvesize: valveSize = {sizeId:0, size:0, eoa: 0.0};
 
   constructor(private alertify: AlertifyService,
     private prod: ProductService,
@@ -26,9 +29,7 @@ export class EditProductComponent implements OnInit {
 
 
   ngOnInit(): void {
-
     this.uploader = new FileUploader({
-
     });
 
   }
@@ -43,11 +44,45 @@ export class EditProductComponent implements OnInit {
       this.povOut.emit(1);
     }, (error) => { console.log(error); });
   }
+  addSize(){
+    this.alertify.message("open the add window"); 
+    this.showAdd = 1;
+    this.alertify.message("opening window");
+  }
+  saveSize(){
+     // close the add window
+     this.showAdd = 0;
+     this.valvesize.size = this.newsize;
+     this.valvesize.eoa = this.neweoa;
+     this.prod.addValveSize(this.vc.valveTypeId, this.valvesize).subscribe((next)=>{
+          // get the changed valveType
+      this.prod.getProductById(this.vc.valveTypeId).subscribe((next)=>{
+        this.newsize = 0; 
+        this.neweoa = 0.0;
+        this.alertify.message("uploading size");
+        this.vc = next;
+      });
+   
+       
+       
+
+     })
+     
+  }
+  deleteSize(id:number){
+    this.prod.deleteValveSize(this.vc.valveTypeId, id).subscribe((next)=>{
+      this.alertify.message("size removed ...");
+      // get the changed valveType
+      this.prod.getProductById(this.vc.valveTypeId).subscribe((next)=>{this.vc = next});
+    })
+   
+  }
+
+  displayAdd(){if(this.showAdd === 1){return true;}}
 
   cancel() {
     this.povOut.emit(1);
   }
-
   deleteProduct() {
     this.alertify.confirm('Are you sure ?', () => {
       this.prod.deleteProduct(this.vc.no).subscribe();

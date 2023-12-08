342eaa1a8edc05efcb360ac28654547fe72b7479
marcel <marcelharder@protonmail.com>
2023-12-08 10:15:24 +0100

    current contact works

--- spa/src/app/superuser/superManageContacts/superManageContacts.component.ts
+++ spa/src/app/superuser/superManageContacts/superManageContacts.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Hospital } from 'src/app/_models/Hospital';
+import { DropItem } from 'src/app/_models/dropItem';
 import { AlertifyService } from 'src/app/_services/alertify.service';
+import { UserService } from 'src/app/_services/user.service';
 
 @Component({
   selector: 'app-superManageContacts',
@@ -9,14 +11,24 @@ import { AlertifyService } from 'src/app/_services/alertify.service';
 })
 export class SuperManageContactsComponent implements OnInit {
   @Input() selectedHospital: Hospital;
+  contactNames:DropItem[];
+  current_contact_username = "";
+  current_contact_image = "";
+  
 
-  constructor(private alertify: AlertifyService) { }
+  constructor(private alertify: AlertifyService, private userService: UserService) { }
 
   ngOnInit() {
-    
-  }
-  ContactTitle() { return "adding"; }
+    // get username from contact
+    this.userService.getUser(+this.selectedHospital.contact).subscribe((next)=>{
+      this.current_contact_username = next.username;
+      this.current_contact_image = next.photoUrl;
+    });
 
-  addContact() { this.alertify.message("Adding contact") }
+    // get contacts for this hospital
+  }
+  ContactTitle() { return "Hier komt de contact page title"; }
+
+  
 
 }
